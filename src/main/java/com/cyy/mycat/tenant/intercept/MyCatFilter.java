099184c9472ac95cbdb5a51bf6f052e3e3d50b82
姚嘉鑫 <yaojiaxin@eccom.com.cn>
2019-12-31 14:33:44 +0800

    commit

---
+++ src/main/java/com/cyy/mycat/tenant/intercept/MyCatFilter.java
@@ -0,0 +1,71 @@
+package com.cyy.mycat.tenant.intercept;
+
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+
+public class MyCatFilter implements Filter {
+
+	private static final ThreadLocal<String> SCHEMA_LOCAL = new ThreadLocal<>();
+	private static final ThreadLocal<String> TENANT_ID_LOCAL = new ThreadLocal<>();
+	public static String getSchema() {
+		String schema = SCHEMA_LOCAL.get();
+		if (!StringUtils.isEmpty(schema)) {
+			return schema;
+		} else {
+			return "";
+		}
+	}
+
+	public static String getTenantId(){
+		String tenantId=TENANT_ID_LOCAL.get();
+		if (!StringUtils.isEmpty(tenantId)) {
+			return tenantId;
+		} else {
+			return "";
+		}
+	}
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+		try {
+			HttpServletRequest req = (HttpServletRequest) request;
+			String tenantId = req.getParameter("tenant_id");
+			//根据租户id查询租户对应的数据库
+			if (!StringUtils.isEmpty(tenantId)) {
+				// 静态的本地线程变量来存储租户信息
+				TENANT_ID_LOCAL.set(tenantId);
+				// TODO 根据租户id查询schema逻辑数据库  从redis或者数据库中查询
+				String schema = null;
+				// 静态的本地线程变量来存储数据库信息
+				SCHEMA_LOCAL.set(schema);
+				// chain
+				chain.doFilter(request, response);
+			} else {
+				chain.doFilter(request, response);
+			}
+		} finally {
+			SCHEMA_LOCAL.remove();
+		}
+
+	}
+
+	@Override
+	public void destroy() {
+	}
+
+}
---
+++ src/main/java/com/cyy/mycat/tenant/intercept/MyInterceptor.java
@@ -0,0 +1,58 @@
+package com.cyy.mycat.tenant.intercept;
+
+import java.sql.Connection;
+import java.util.Properties;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.executor.statement.RoutingStatementHandler;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+
+
+@Intercepts({
+		@Signature(method = "query", type = Executor.class, args = {
+				MappedStatement.class, Object.class, RowBounds.class,
+				ResultHandler.class }),
+		@Signature(method = "prepare", type = StatementHandler.class, args = { Connection.class }) })
+public class MyInterceptor implements Interceptor {
+
+	private final static String[] scrm_public = { "public_tenant"};
+	@Override
+	public Object intercept(Invocation invocation) throws Throwable {
+		String schema = MyCatFilter.getSchema();
+		if (invocation.getTarget() instanceof RoutingStatementHandler) {
+			RoutingStatementHandler statementHandler = (RoutingStatementHandler) invocation
+					.getTarget();
+			StatementHandler delegate = (StatementHandler) ReflectHelper
+					.getFieldValue(statementHandler, "delegate");
+			BoundSql boundSql = delegate.getBoundSql();
+			Object obj = boundSql.getParameterObject();
+			// 通过反射获取delegate父类BaseStatementHandler的mappedStatement属性
+			MappedStatement mappedStatement = (MappedStatement) ReflectHelper
+					.getFieldValue(delegate, "mappedStatement");
+			// 获取当前要执行的Sql语句，也就是我们直接在Mapper映射语句中写的Sql语句
+			String sql = boundSql.getSql();
+			String NowSql = "/*!mycat:schema = " + schema + " */  " + sql;
+			System.out.println(NowSql);
+			// 利用反射设置当前BoundSql对应的sql属性为我们修改完的Sql语句
+			ReflectHelper.setFieldValue(boundSql, "sql", NowSql);
+		}
+		return invocation.proceed();
+	}
+    @Override
+	public Object plugin(Object target) {
+		return Plugin.wrap(target, this);
+	}
+	@Override
+	public void setProperties(Properties properties) {
+	}
+
+}
